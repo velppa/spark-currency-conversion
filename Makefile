5c75b08d5a624fc74af94b7983efcc92e51646cf
Pavel Popov <pavel@findhotel.net>
2021-07-19 01:22:32 +0200

    Add stream processing

--- Makefile
+++ Makefile
@@ -3,10 +3,26 @@ RATES=rates.csv
 load-rates:
 	clj -X rates/-main :filename '"$(RATES)"'
 
-submit:
+
+INPUT=transactions.csv
+OUTPUT_DIR=results/batch
+run-batch:
+	rm -rf $(OUTPUT_DIR)
 	spark-submit \
-	  --conf spark.driver.extraClassPath=./  \
-      --conf spark.executor.extraClassPath=./ \
+	  --class com.example.transactions.TransactionEnricherBatch \
 	  target/scala-2.12/spark-currency-conversion-assembly-0.1.jar \
-	  --input transactions.csv \
-	  --rates $(RATES)
+	  --input $(INPUT) \
+	  --rates $(RATES) \
+	  --output-dir $(OUTPUT_DIR)
+
+
+INPUT_DIR=stream
+run-stream:
+	mkdir -p $(INPUT_DIR)
+	rm -rf $(OUTPUT_DIR)
+	spark-submit \
+	  --class com.example.transactions.TransactionEnricherStream \
+	  target/scala-2.12/spark-currency-conversion-assembly-0.1.jar \
+	  --input-dir $(INPUT_DIR) \
+	  --rates $(RATES) \
+	  --output-dir $(OUTPUT_DIR)
--- src/main/scala/com/example/transactions/MainJob.scala
+++ src/main/scala/com/example/transactions/MainJob.scala
@@ -3,77 +3,116 @@ package com.example.transactions
 import com.twitter.scalding.Args
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
-object JobConfig {
-  val config = ConfigFactory.load
-  lazy val sparkMaster = config.getString("MainJob.spark.master")
+object Schemas {
+  val transaction = new StructType()
+       .add("transaction_id",IntegerType,true)
+       .add("customer_id",IntegerType,true)
+       .add("merchant_id",IntegerType,true)
+       .add("card_id",StringType,true)
+       .add("timestamp",IntegerType,true)
+       .add("currency",StringType,true)
+       .add("amount",DoubleType,true)
+
+  val rate = new StructType()
+       .add("currency",StringType,true)
+       .add("rate",DoubleType,true)
+       .add("created_at",IntegerType,true)
+       .add("effective_from",IntegerType,true)
+       .add("effective_to",IntegerType,true)
 }
 
-object MainJob extends LazyLogging {
+object Enricher {
+  def withAmountUSD(transactions: DataFrame, rates: DataFrame): DataFrame = {
+    val interval = 4 * 7 * 24 * 60 * 60
 
-  import JobConfig._
+    transactions
+     .withColumn("effective_from", col("timestamp") - col("timestamp") % lit(interval))
+     .join(rates, Seq("currency", "effective_from"))
+     .withColumn("amount_usd", col("amount")*col("rate"))
+     .withColumnRenamed("rate", "fx")
+     .withColumnRenamed("effective_from", "fx_from")
+     .drop("effective_to", "created_at")
+  }
+}
 
-  val transactionSchema = new StructType()
-      .add("transaction_id",IntegerType,true)
-      .add("customer_id",IntegerType,true)
-      .add("merchant_id",IntegerType,true)
-      .add("card_id",StringType,true)
-      .add("timestamp",IntegerType,true)
-      .add("currency",StringType,true)
-      .add("amount",DoubleType,true)
-
-  val rateSchema = new StructType()
-      .add("currency",StringType,true)
-      .add("rate",DoubleType,true)
-      .add("created_at",IntegerType,true)
-      .add("effective_from",IntegerType,true)
-      .add("effective_to",IntegerType,true)
+object TransactionEnricherBatch extends LazyLogging {
+  val config = ConfigFactory.load.getConfig("TransactionEnricherBatch")
 
   def main(args: Array[String]): Unit = {
-    logger.debug(this.getClass.toString)
-
     val params = Args(args)
     val inputFile = params.required("input")
     val ratesFile = params.required("rates")
+    val outputDir = params.required("output-dir")
 
     logger.debug(s"inputFile=$inputFile, ratesFile=$ratesFile")
 
     val sparkConf = new SparkConf()
-      .setMaster(sparkMaster)
+      .setMaster(config.getString("spark.master"))
       .setAppName(this.getClass.toString)
 
     implicit val spark = SparkSession.builder.config(sparkConf).getOrCreate()
     try {
-
       val transactions = spark.read.format("csv")
         .option("header", true)
-        .schema(transactionSchema)
+        .schema(Schemas.transaction)
         .load(inputFile)
 
       val rates = spark.read.format("csv")
-        .option("header", true)
-        .schema(rateSchema)
+        .option("header", false)
+        .schema(Schemas.rate)
         .load(ratesFile)
 
       transactions.show
       rates.show
 
-      val interval = 4 * 7 * 24 * 60 * 60
+      Enricher.withAmountUSD(transactions, rates)
+        .repartition(1)
+        .write
+        .option("header", true)
+        .csv(outputDir)
+    }
+    finally {
+      spark.close()
+    }
+  }
+}
 
-      transactions
-        .withColumn("effective_from", col("timestamp") - col("timestamp") % lit(interval))
-        .join(rates, Seq("currency", "effective_from"))
-        .withColumn("amount_usd", col("amount")*col("rate"))
-        .withColumnRenamed("rate", "fx")
-        .withColumnRenamed("effective_from", "fx_from")
-        .drop("effective_to", "created_at")
-        .show
+object TransactionEnricherStream extends LazyLogging {
+  val config = ConfigFactory.load.getConfig("TransactionEnricherStream")
+
+  def main(args: Array[String]): Unit = {
+    val params = Args(args)
+    val transactionsDir = params.required("input-dir")
+    val ratesFile = params.required("rates")
+
+    val sparkConf = new SparkConf()
+      .setMaster(config.getString("spark.master"))
+      .setAppName(this.getClass.toString)
+
+    implicit val spark = SparkSession.builder.config(sparkConf).getOrCreate()
+    try {
+      val transactions = spark.readStream
+        .schema(Schemas.transaction)
+        .csv(transactionsDir)
+
+      val rates = spark.read.format("csv")
+        .option("header", false)
+        .schema(Schemas.rate)
+        .load(ratesFile)
+
+      val enriched = Enricher.withAmountUSD(transactions, rates)
+        .writeStream
+        .format("console")
+        .start()
+
+      enriched.awaitTermination()
     }
     finally {
       spark.close()
